4a6ca255d5db3f6dc83b444ab717dfd4dfed7fca
Vinko Bradvica <vinko.bradvica@gmail.com>
2018-04-30 16:25:38 +0200

    Removed development network config so that tests pass on CircleCI

--- truffle-config.js
+++ truffle-config.js
@@ -2,12 +2,4 @@ require('babel-register');
 require('babel-polyfill');
 
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 7545,
-      network_id: '*', // eslint-disable-line camelcase
-      gasPrice: 5000000000,
-    },
-  },
 };
